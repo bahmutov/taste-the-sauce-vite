72478260465c09729c92b3dd8eef6667d9aa472a
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2024-06-18 09:43:48 -0400

    update the vite demo app

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'
+import IstanbulPlugin from 'vite-plugin-istanbul'
 
 export default defineConfig({
   appType: 'spa',
-  plugins: [react(), svgr()],
+  build: {
+    sourcemap: true,
+  },
+  plugins: [
+    react(),
+    svgr(),
+    IstanbulPlugin({
+      include: 'src/*',
+      exclude: ['node_modules', 'cypress/', '*.cy.js'],
+      extension: ['.js', '.jsx', '.ts', '.tsx'],
+    }),
+  ],
   server: {
     port: 3000,
     host: '127.0.0.1',
