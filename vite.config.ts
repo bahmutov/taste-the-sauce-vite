2017f2c1825bba70cb1ef1a8fcdacb97c9f87779
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2024-06-18 09:44:54 -0400

    small tweaks

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'
-import IstanbulPlugin from 'vite-plugin-istanbul'
 
 export default defineConfig({
   appType: 'spa',
   build: {
     sourcemap: true,
   },
-  plugins: [
-    react(),
-    svgr(),
-    IstanbulPlugin({
-      include: 'src/*',
-      exclude: ['node_modules', 'cypress/', '*.cy.js'],
-      extension: ['.js', '.jsx', '.ts', '.tsx'],
-    }),
-  ],
+  plugins: [react(), svgr()],
   server: {
     port: 3000,
     host: '127.0.0.1',
