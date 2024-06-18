3f2b747fe45544e5ac611aa0ff1b2aea6f74ab3c
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2024-06-18 09:53:14 -0400

    initial code

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'
+// https://github.com/iFaxity/vite-plugin-istanbul
+import IstanbulPlugin from 'vite-plugin-istanbul'
 
 export default defineConfig({
   appType: 'spa',
   build: {
     sourcemap: true,
   },
+  // TODO: include the IstanbulPlugin in the plugins array
+  // to instrument the bundled code
   plugins: [react(), svgr()],
   server: {
     port: 3000,
