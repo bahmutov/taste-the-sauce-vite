ae80b1a3fa7441b9317d23c1674e19063d8b7014
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2024-07-07 13:00:59 -0400

    initial code

--- cypress/e2e/login/token.cy.ts
+++ cypress/e2e/login/token.cy.ts
@@ -5,15 +5,10 @@ it('saves the access token', () => {
   LoginPage.getUsername().type('standard_user')
   LoginPage.getPassword().type('secret_sauce')
   LoginPage.getLogin().click()
-  cy.location('pathname').should('equal', '/inventory')
 
   // the application saves the current username
   // in the local storage key "user_session_name"
   // check that it is saved
   // https://on.cypress.io/window
   // https://on.cypress.io/then
-  cy.window().then((win) => {
-    const user = win.localStorage.getItem('user_session_name')
-    expect(user, 'username in local storage').to.eq('standard_user')
-  })
 })
