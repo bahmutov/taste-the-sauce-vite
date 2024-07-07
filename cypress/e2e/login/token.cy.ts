0f0b4317690ebf8abbac562c75bdd8c4ba14d9bb
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2024-07-07 13:10:14 -0400

    use the should callback

--- cypress/e2e/login/token.cy.ts
+++ cypress/e2e/login/token.cy.ts
@@ -12,7 +12,7 @@ it('saves the access token', () => {
   // check that it is saved
   // https://on.cypress.io/window
   // https://on.cypress.io/then
-  cy.window().then((win) => {
+  cy.window().should((win) => {
     const user = win.localStorage.getItem('user_session_name')
     expect(user, 'username in local storage').to.eq('standard_user')
   })
