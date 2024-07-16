5e04a4af16da25b7d16938b2de870f1fa6067559
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2024-07-16 09:31:27 -0400

    bonus 62 starting code

--- cypress/e2e/login/badge.cy.ts
+++ cypress/e2e/login/badge.cy.ts
@@ -11,18 +11,8 @@ it('shows the "new" badge', () => {
   // confirm this item has the badge "NEW" after it
   // Tip: see how pseudo-elements like "::after" can be tested
   // https://glebbahmutov.com/cypress-examples
-  cy.contains('.inventory_item_name', newItemTitle).then(($el) => {
-    const after = window.getComputedStyle($el[0], '::after')
-    const afterContent = after.getPropertyValue('content')
-    expect(afterContent).to.equal('"NEW"')
-  })
 
   // Part 2: refactor the above code to check
   // the "::after" badge content using queries
   // from cypress-map plugin
-  cy.log('**using cypress-map**')
-  cy.contains('.inventory_item_name', newItemTitle)
-    .applyToFirstRight(window.getComputedStyle, '::after')
-    .invoke('getPropertyValue', 'content')
-    .should('equal', '"NEW"')
 })
