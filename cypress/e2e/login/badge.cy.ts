12d81b95728be17bfb5808a4209f9554720dc4a6
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2024-07-16 13:08:09 -0400

    working

--- cypress/e2e/login/badge.cy.ts
+++ cypress/e2e/login/badge.cy.ts
@@ -1,5 +1,19 @@
 import { LoginPage } from '@support/pages/login.page'
 
+// a couple of helper functions for simplicity and readability
+
+function haveRedDotBadge($el: JQuery<HTMLElement>) {
+  const badge = window.getComputedStyle($el[0], '::after')
+  const backgroundColor = badge.getPropertyValue('background-color')
+  expect(backgroundColor, 'red dot').to.equal('rgb(226, 35, 26)')
+}
+
+function notHaveDotBadge($el: JQuery<HTMLElement>) {
+  const badge = window.getComputedStyle($el[0], '::after')
+  const backgroundColor = badge.getPropertyValue('background-color')
+  expect(backgroundColor, 'no dot').to.equal('rgba(0, 0, 0, 0)')
+}
+
 // make the page taller so we can see the entire inventory
 // plus the header component with the cart badge
 it('shows the "item in the cart" badge', { viewportHeight: 1600 }, () => {
@@ -15,6 +29,11 @@ it('shows the "item in the cart" badge', { viewportHeight: 1600 }, () => {
     .click()
 
   // verify the cart icon has a red dot badge (after possible delay)
+  cy.get('.shopping_cart_link').should(haveRedDotBadge)
+
   // remove the item from the cart by clicking the "Remove" button
+  cy.contains('.inventory_item', itemName).contains('button', 'Remove').click()
+
   // and verify the cart icon badge is removed (after possible delay)
+  cy.get('.shopping_cart_link').should(notHaveDotBadge)
 })
