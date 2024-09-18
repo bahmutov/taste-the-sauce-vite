b0379b172fda79ffd3fa1019dc952903a4bc79af
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2024-09-18 09:10:21 -0400

    solution

--- cypress/e2e/prices.cy.ts
+++ cypress/e2e/prices.cy.ts
@@ -1,13 +1,19 @@
 import { LoginInfo } from '.'
 import { LoginPage } from '@support/pages/login.page'
 
+// create a "branded type" that represents price in cents
+// note that it won't be compatible by default with numbers
+// so we need to cast numbers to this type
+// see https://www.learningtypescript.com/articles/branded-types
+type cents = number & { __brandType: 'cents' }
+
 /**
  * Returns a string with the formatted price.
  * The input is price in cents.
  * @example
  *  formatPrice(1000) // returns '$10.00'
  */
-function formatPrice(price: number) {
+function formatPrice(price: cents) {
   return '$' + (price / 100).toFixed(2)
 }
 
@@ -19,7 +25,9 @@ describe('Prices', { viewportHeight: 1200, scrollBehavior: false }, () => {
     cy.visit('/inventory')
     // we could pass any number for `formatPrice`
     // and the type check would still work 🤔
-    const n = 2999
+    // declare the price as cents using "<number> as cents"
+    // to be able to pass it to "formatPrice" function
+    const n = 2999 as cents
     cy.contains('.inventory_item_price', formatPrice(n))
   })
 })
