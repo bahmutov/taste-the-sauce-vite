43914bc8bb456a88da6104463f40337d84bd8f4f
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2025-02-16 17:02:38 -0500

    remove other code

--- src/components/Button.cy.jsx
+++ src/components/Button.cy.jsx
@@ -4,7 +4,5 @@ import Button from './Button'
 it('shows a button', () => {
   // use the cy.mount command to mount the Button component
   // with the prop `label` set to 'Test button'
-  cy.mount(<Button label="Test button" />)
   // confirm the page contains a button with the text 'Test button'
-  cy.contains('button', 'Test button')
 })
