a11809649df68206f09cc243bb6b89a643104a45
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2025-02-16 17:00:26 -0500

    answer

--- src/components/Button.cy.jsx
+++ src/components/Button.cy.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import Button from './Button'
 
 it('shows a button', () => {
+  // use the cy.mount command to mount the Button component
+  // with the prop `label` set to 'Test button'
   cy.mount(<Button label="Test button" />)
+  // confirm the page contains a button with the text 'Test button'
   cy.contains('button', 'Test button')
 })
