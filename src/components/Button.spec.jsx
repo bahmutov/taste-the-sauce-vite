b4edb63757db81811b3542aa016b516552063309
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2025-02-16 17:29:31 -0500

    initial code

---
+++ src/components/Button.spec.jsx
@@ -0,0 +1,8 @@
+import { test, expect } from '@playwright/experimental-ct-react17'
+import Button from './Button'
+
+test('shows a button', async ({ mount }) => {
+  // use the mount command to mount the Button component
+  // with the prop `label` set to 'Test button'
+  // confirm the component contains text 'Test button'
+})
