fef19f4e6696ad4816d6b0c1fc9c603d5805135b
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2025-02-17 11:46:06 -0500

    add placeholder

--- src/components/Button.cy.jsx
+++ src/components/Button.cy.jsx
@@ -6,3 +6,14 @@ it('shows a button', () => {
   // with the prop `label` set to 'Test button'
   // confirm the page contains a button with the text 'Test button'
 })
+
+it('passes custom class name', () => {
+  // mount the Button with the customClass prop set to "myClass"
+  // confirm the page contains a button with the class "myClass"
+})
+
+it('sets the test id', () => {
+  // mount the Button with the testId prop set to "myTestId"
+  // confirm the button with the text "Test button" has
+  // the data-test, name, and id set to "myTestId"
+})
