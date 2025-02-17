d4cef338f8c64cf6684e6c76139838210d95a327
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2025-02-17 11:44:33 -0500

    more tests

--- src/components/Button.cy.jsx
+++ src/components/Button.cy.jsx
@@ -8,3 +8,21 @@ it('shows a button', () => {
   // confirm the page contains a button with the text 'Test button'
   cy.contains('button', 'Test button')
 })
+
+it('passes custom class name', () => {
+  // mount the Button with the customClass prop set to "myClass"
+  cy.mount(<Button label="Test button" customClass="myClass" />)
+  // confirm the page contains a button with the class "myClass"
+  cy.get('button.myClass')
+})
+
+it('sets the test id', () => {
+  // mount the Button with the testId prop set to "myTestId"
+  cy.mount(<Button label="Test button" testId="myTestId" />)
+  // confirm the button with the text "Test button" has
+  // the data-test, name, and id set to "myTestId"
+  cy.contains('button', 'Test button')
+    .should('have.id', 'myTestId')
+    .and('have.attr', 'name', 'myTestId')
+    .and('have.attr', 'data-test', 'myTestId')
+})
