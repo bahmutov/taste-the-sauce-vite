d2f99e18f3d545bfb6e1bc017a5c20e880a23e10
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2025-02-18 21:11:17 -0500

    expect

--- src/components/Button.spec.jsx
+++ src/components/Button.spec.jsx
@@ -65,5 +65,5 @@ test('callback prop is called on click', async ({ mount }) => {
   await component.click()
   // confirm the mock function was called
   // by checking if the "clicked" state is true
-  await expect(clicked, 'clicked').toBeTruthy()
+  expect(clicked, 'clicked').toBeTruthy()
 })
