fb0c6b531e15e080b73f858a43c1b8c086da9b07
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2025-02-20 10:50:19 -0500

    more comments

--- src/components/Button.cy.jsx
+++ src/components/Button.cy.jsx
@@ -3,5 +3,8 @@ import Button from './Button'
 
 it('renders a button on green background', () => {
   cy.mount(<Button label="Green" />)
+  // confirm the button has a green assertion
+  // Tip: use the "have.css" Chai-jQuery assertion
+  // https://www.chaijs.com/plugins/chai-jquery/
   cy.get('button').should('have.css', 'background-color', 'rgb(0, 128, 0)')
 })
--- src/components/Button.spec.jsx
+++ src/components/Button.spec.jsx
@@ -3,5 +3,8 @@ import Button from './Button'
 
 test('renders a button on green background', async ({ mount }) => {
   const component = await mount(<Button label="Green" />)
+  // confirm the button has a green background
+  // Tip: use "toHaveCSS" assertion
+  // https://playwright.dev/docs/api/class-locatorassertions
   await expect(component).toHaveCSS('background-color', 'rgb(0, 128, 0)')
 })
