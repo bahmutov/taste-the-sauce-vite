d3a4e4433606b877b0214e720be0430f3dead152
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2025-02-21 10:07:50 -0500

    add second pw test

--- src/components/InventoryListItem.spec.jsx
+++ src/components/InventoryListItem.spec.jsx
@@ -21,3 +21,29 @@ test('InventoryListItem loads one item', async ({ mount }) => {
   // assert that the item's name is there
   await expect(component.locator('.inventory_item_name')).toHaveText(item.name)
 })
+
+// Helper function to mount the component inside a router
+// to avoid repeating the router setup in each test
+// Note: in the real application, you probably would use Playwright Test Fixtures
+// https://playwright.dev/docs/test-fixtures
+function mountWithRouter(mountFn, component) {
+  return mountFn(
+    <BrowserRouter initialEntries={[]}>
+      <Route>{component}</Route>
+    </BrowserRouter>,
+  )
+}
+
+test('InventoryListItem loads one item (mount helper)', async ({ mount }) => {
+  // rewrite the above test to use the "mountWithRouter" helper function
+  // to avoid repeating the router setup
+  const item = InventoryData[3]
+  const component = await mountWithRouter(
+    mount,
+    <InventoryListItem {...item} />,
+  )
+  // confirm the component is visible
+  // and has the correct item name
+  await expect(component).toBeVisible()
+  await expect(component.locator('.inventory_item_name')).toHaveText(item.name)
+})
