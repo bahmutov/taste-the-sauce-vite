38a134917625383bc5f8d1a53e81f4a439821f86
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2025-03-13 10:37:54 -0400

    add to the component

--- cypress/support/component.jsx
+++ cypress/support/component.jsx
@@ -3,6 +3,9 @@ import 'cypress-map'
 import { Route, BrowserRouter } from 'react-router-dom'
 import { mount } from 'cypress/react'
 
+// https://www.npmjs.com/package/cypress-plugin-steps
+import 'cypress-plugin-steps'
+
 // https://github.com/bahmutov/cypress-code-coverage
 import '@bahmutov/cypress-code-coverage/support'
 
