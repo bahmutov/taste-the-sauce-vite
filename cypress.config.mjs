7e88be82f6d7d269088e47feb08ddbc653c0ff65
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2025-05-22 15:51:35 -0400

    playing with changes

--- cypress.config.mjs
+++ cypress.config.mjs
@@ -1,18 +1,18 @@
 import { defineConfig } from 'cypress'
-// import registerDataSession from 'cypress-data-session/src/plugin'
-// // https://github.com/bahmutov/cypress-split
-// import cypressSplit from 'cypress-split'
-// // https://github.com/bahmutov/cypress-on-fix
-// import cypressOnFix from 'cypress-on-fix'
-// import path from 'path'
-// // https://github.com/bahmutov/cypress-watch-and-reload
-// import reloadWatch from 'cypress-watch-and-reload/plugins'
-// // https://github.com/bahmutov/cypress-code-coverage
-// import codeCoveragePlugin from '@bahmutov/cypress-code-coverage/plugin'
-// // https://github.com/iFaxity/vite-plugin-istanbul
-// import IstanbulPlugin from 'vite-plugin-istanbul'
-// // https://github.com/bahmutov/cypress-visited-urls
-// import visitedUrlsPlugin from 'cypress-visited-urls/src/plugin'
+import registerDataSession from 'cypress-data-session/src/plugin'
+// https://github.com/bahmutov/cypress-split
+import cypressSplit from 'cypress-split'
+// https://github.com/bahmutov/cypress-on-fix
+import cypressOnFix from 'cypress-on-fix'
+import path from 'path'
+// https://github.com/bahmutov/cypress-watch-and-reload
+import reloadWatch from 'cypress-watch-and-reload/plugins'
+// https://github.com/bahmutov/cypress-code-coverage
+import codeCoveragePlugin from '@bahmutov/cypress-code-coverage/plugin'
+// https://github.com/iFaxity/vite-plugin-istanbul
+import IstanbulPlugin from 'vite-plugin-istanbul'
+// https://github.com/bahmutov/cypress-visited-urls
+import visitedUrlsPlugin from 'cypress-visited-urls/src/plugin'
 
 const __dirname = import.meta.dirname
 
@@ -58,17 +58,17 @@ export default defineConfig({
     },
     setupNodeEvents(cypressOn, config) {
       // fix https://github.com/cypress-io/cypress/issues/22428
-      // const on = cypressOnFix(cypressOn)
-      // // implement node event listeners here
-      // // and load any plugins that require the Node environment
-      // cypressSplit(on, config)
-      // registerDataSession(on, config)
-      // reloadWatch(on, config)
-      // codeCoveragePlugin(on, config)
-      // visitedUrlsPlugin(on, config)
-      // // IMPORTANT to return the config object
-      // // with the any changed environment variables
-      // return config
+      const on = cypressOnFix(cypressOn)
+      // implement node event listeners here
+      // and load any plugins that require the Node environment
+      cypressSplit(on, config)
+      registerDataSession(on, config)
+      reloadWatch(on, config)
+      codeCoveragePlugin(on, config)
+      visitedUrlsPlugin(on, config)
+      // IMPORTANT to return the config object
+      // with the any changed environment variables
+      return config
     },
   },
 
