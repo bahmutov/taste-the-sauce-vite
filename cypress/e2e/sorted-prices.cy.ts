a42079438114324a8237e77e26692f09b8e9aeaa
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2025-12-29 22:21:46 -0500

    add sorted spec

---
+++ cypress/e2e/sorted-prices.cy.ts
@@ -0,0 +1,25 @@
+import { LoginPage } from '@support/pages/login.page'
+
+describe('Inventory page', () => {
+  // visit the login page before each test
+  beforeEach(() => {
+    const user = Cypress.env('users').standard
+    LoginPage.login(user.username, user.password)
+  })
+
+  it('sorts items by price, low to high', () => {
+    cy.visit('/inventory')
+    cy.get('[data-test="product_sort_container"]').select('Price (low to high)')
+    cy.get('.inventory_item_price')
+      .map('innerText')
+      // slice the leading $
+      .mapInvoke('slice', 1)
+      .map(parseFloat)
+      .print('prices %o')
+      // confirm the numbers are sorted in ascending order
+      .then((prices) => {
+        const sortedPrices = [...prices].sort((a, b) => a - b)
+        expect(prices).to.deep.equal(sortedPrices)
+      })
+  })
+})
